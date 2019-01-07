dcc37b481d90435265b97d63218d27a68dfdf689
Devin Herath <devdhera@gmail.com>
2019-01-07 19:20:26 +0530

    Added Tests

---
+++ src/test/java/com/devin/filerestmysql/FileRestMysqlApplicationTests.java
@@ -0,0 +1,17 @@
+package com.devin.filerestmysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileRestMysqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
