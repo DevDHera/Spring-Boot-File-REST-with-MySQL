d3d465b11a61d793d32487dbc0b32527548c1861
Devin Herath <devdhera@gmail.com>
2019-01-07 19:22:01 +0530

    Added Main class

---
+++ src/main/java/com/devin/filerestmysql/FileRestMysqlApplication.java
@@ -0,0 +1,14 @@
+package com.devin.filerestmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileRestMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileRestMysqlApplication.class, args);
+    }
+
+}
+
